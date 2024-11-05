6f7ccea90b7dabcd74bb6592bc46f9d40283127f
ITSVAPP <it.da.fukase@iftc.co.jp>
2024-11-05 18:49:58 +0900

    @buildのbugに対する暫定対策

--- line-bot-model/src/main/java/com/linecorp/bot/model/richmenu/RichMenuBlukUnlinkRequest.java
+++ line-bot-model/src/main/java/com/linecorp/bot/model/richmenu/RichMenuBlukUnlinkRequest.java
@@ -19,12 +19,11 @@ package com.linecorp.bot.model.richmenu;
 import java.util.List;
 
 import lombok.Builder;
-import lombok.Singular;
 import lombok.Value;
 
 @Value
 @Builder
 public class RichMenuBlukUnlinkRequest {
-    @Singular
+    
     List<String> userIds;
 }
