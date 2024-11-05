a6dc455e8924232ba88900b7b97b50aa5cf7d9d5
ITSVAPP <it.da.fukase@iftc.co.jp>
2024-11-05 18:49:58 +0900

    @buildのbugに対する暫定対策

--- line-bot-model/src/main/java/com/linecorp/bot/model/richmenu/RichMenuBlukLinkRequest.java
+++ line-bot-model/src/main/java/com/linecorp/bot/model/richmenu/RichMenuBlukLinkRequest.java
@@ -19,13 +19,12 @@ package com.linecorp.bot.model.richmenu;
 import java.util.List;
 
 import lombok.Builder;
-import lombok.Singular;
 import lombok.Value;
 
 @Value
 @Builder
 public class RichMenuBlukLinkRequest {
     String richMenuId;
-    @Singular
+    
     List<String> userIds;
 }
